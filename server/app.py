3249a822824bbbd2a9a31f41cef1c5ae1b66c8cd
Peter Njiru <peternjirum@gmail.com>
2023-09-15 23:57:34 -0400

    completes lab

--- server/app.py
+++ server/app.py
@@ -18,23 +18,20 @@ db.init_app(app)
 
 api = Api(app)
 
-
 class ClearSession(Resource):
 
     def delete(self):
-
+    
         session['page_views'] = None
         session['user_id'] = None
 
         return {}, 204
 
-
 class IndexArticle(Resource):
-
+    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return make_response(jsonify(articles), 200)
-
 
 class ShowArticle(Resource):
 
@@ -44,8 +41,7 @@ class ShowArticle(Resource):
         article_json = article.to_dict()
 
         if not session.get('user_id'):
-            session['page_views'] = 0 if not session.get(
-                'page_views') else session.get('page_views')
+            session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
             session['page_views'] += 1
 
             if session['page_views'] <= 3:
@@ -55,42 +51,38 @@ class ShowArticle(Resource):
 
         return article_json, 200
 
-
 class Login(Resource):
 
     def post(self):
-
+        
         username = request.get_json().get('username')
         user = User.query.filter(User.username == username).first()
 
         if user:
-
+        
             session['user_id'] = user.id
             return user.to_dict(), 200
 
         return {}, 401
-
 
 class Logout(Resource):
 
     def delete(self):
 
         session['user_id'] = None
-
+        
         return {}, 204
-
 
 class CheckSession(Resource):
 
     def get(self):
-
+        
         user_id = session['user_id']
         if user_id:
             user = User.query.filter(User.id == user_id).first()
             return user.to_dict(), 200
-
+        
         return {}, 401
-
 
 class MemberOnlyIndex(Resource):
 
@@ -104,20 +96,18 @@ class MemberOnlyIndex(Resource):
             Article.is_member_only == True).all()]
         return make_response(jsonify(articles), 200)
 
-
 class MemberOnlyArticle(Resource):
-
+    
     def get(self, id):
 
         # check if logged in first so as to display exclusive content
         if not session['user_id']:
-            return {'error': 'Unauthorized'}, 401
+            return {'error': 'Unauthorized'}, 401        
 
         article = Article.query.filter(Article.id == id).first()
-
         article_json = article.to_dict()
-        return article_json, 200
 
+        return article_json, 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
@@ -125,10 +115,8 @@ api.add_resource(ShowArticle, '/articles/<int:id>', endpoint='show_article')
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
-api.add_resource(MemberOnlyIndex, '/members_only_articles',
-                 endpoint='member_index')
-api.add_resource(MemberOnlyArticle,
-                 '/members_only_articles/<int:id>', endpoint='member_article')
+api.add_resource(MemberOnlyIndex, '/members_only_articles', endpoint='member_index')
+api.add_resource(MemberOnlyArticle, '/members_only_articles/<int:id>', endpoint='member_article')
 
 
 if __name__ == '__main__':
